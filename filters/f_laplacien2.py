3dd167b3e481ca42af268f554ced8fa6cbfdb349
christina_mrn <maurin.christina@gmail.com>
2023-11-08 15:31:08 +0100

    maj

--- filters/f_laplacien2.py
+++ filters/f_laplacien2.py
@@ -3,10 +3,11 @@ import sys
 import os
 import matplotlib.pyplot as plt
 
+#Laplacien mais modifié
 
 # Vérification des arguments
 if len(sys.argv) != 2:
-    print("Utilisation : python f_laplacien.py le_chemin.jpg")
+    print("Utilisation : python f_laplacien2.py le_chemin.jpg")
     sys.exit(1)
 
 image_path = sys.argv[1]
@@ -17,10 +18,21 @@ image = Image.open(image_path)
 # Générer le filtre Laplacien pour accentuer les contours
 laplacian_filter = image.filter(ImageFilter.FIND_EDGES)
 
+threshold = 50
+laplacian_filter = laplacian_filter.point(lambda p: p if p > threshold else 0)
+
+largeur, hauteur = laplacian_filter.size
+for x in range(largeur):
+    for y in range(hauteur):
+        if x < 1 or x >= largeur - 1 or y < 1 or y >= hauteur - 1:
+            laplacian_filter.putpixel((x, y), 0)
+
+laplacian_filter = laplacian_filter.filter(ImageFilter.BoxBlur(radius=1))
+
 # Afficher l'image seuillée dans une fenêtre
 plt.imshow(laplacian_filter, cmap='gray')
 plt.title("Image Seuillée")
-plt.axis('off')  # Désactiver les axes
+plt.axis('off')
 plt.show()
 
 # Appliquer filtre
@@ -30,7 +42,7 @@ filtered_image = ImageChops.add(image, laplacian_filter, scale=1.0, offset=0)
 nom_fichier_base, extension = os.path.splitext(os.path.basename(image_path))
 
 # Création du nouveau nom de fichier pour l'image de sortie
-nom_image_sortie = "../imgoutdenoised/" + nom_fichier_base + "_laplacien.jpg"
+nom_image_sortie = "../imgoutdenoised/" + nom_fichier_base + "_laplacien2.jpg"
 
 # Enregistrement de l'image modifiée
 filtered_image.save(nom_image_sortie)
