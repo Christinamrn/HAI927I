dfa4260125e714a44f9433718cc3604065c4fb24
Christinamrn <58227221+Christinamrn@users.noreply.github.com>
2023-11-26 21:29:19 +0100

    Ajout génération de bruit couleur supplémentaires

--- QtChristalApp/imageGenNoises.py
+++ QtChristalApp/imageGenNoises.py
@@ -37,12 +37,24 @@ def bruit_gaussien(image, ecart_type, MainWindow):
     image = copy.deepcopy(image)
     largeur, hauteur = image.size
 
-    for x in range(largeur):
-        for y in range(hauteur):
-            bruit = int(np.random.normal(0, ecart_type))
-            pixel = image.getpixel((x, y))
-            new_pixel = max(0, min(pixel + bruit, 255))
-            image.putpixel((x, y), new_pixel)
+    if(image.mode == "L"):
+        for x in range(largeur):
+            for y in range(hauteur):
+                bruit = int(np.random.normal(0, ecart_type))
+                pixel = image.getpixel((x, y))
+                new_pixel = max(0, min(pixel + bruit, 255))
+                image.putpixel((x, y), new_pixel)
+    else:
+        for x in range(largeur):
+            for y in range(hauteur):
+                bruit = int(np.random.normal(0, ecart_type))
+                pixel = image.getpixel((x, y))
+                r, g, b = pixel
+                new_r = max(0, min(r + bruit, 255))
+                new_g = max(0, min(g + bruit, 255))
+                new_b = max(0, min(b + bruit, 255))
+                new_pixel = (new_r, new_g, new_b)
+                image.putpixel((x, y), new_pixel)
     enregistrerImageTmpNoisy(image)
     MainWindow.affichageImageNoisy()
 
@@ -51,13 +63,23 @@ def bruit_gaussien(image, ecart_type, MainWindow):
 def bruit_poivre_et_sel(image, densite, MainWindow):
     image = copy.deepcopy(image)
     largeur, hauteur = image.size
+    pixels = image.load()
 
-    for x in range(largeur):
-        for y in range(hauteur):
-            if random.random() < densite:
-                bruit = random.randint(0, 255)
-                new_pixel = min(bruit, 255)
-                image.putpixel((x, y), new_pixel)
+    if(image.mode == "L"):
+        for x in range(largeur):
+            for y in range(hauteur):
+                if random.random() < densite:
+                    bruit = random.randint(0, 255)
+                    new_pixel = min(bruit, 255)
+                    image.putpixel((x, y), new_pixel)
+    else:
+        for x in range(largeur):
+            for y in range(hauteur):
+                if random.random() < densite:
+                    bruit = random.randint(0, 255)
+                    new_pixel = (min(bruit, 255), min(bruit, 255), min(bruit, 255))
+                    pixels[x,y] = new_pixel
+                    #image.putpixel((x, y), new_pixel)
     enregistrerImageTmpNoisy(image)
     MainWindow.affichageImageNoisy()
 
